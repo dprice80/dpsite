25d821640b85082622537bd2d6a47443d371d554
komadmin <komendweb@gmail.com>
2017-03-03 14:46:13 +0000

    Trying to fix static

--- siteroot/settings.py
+++ siteroot/settings.py
@@ -200,8 +200,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            # os.path.join(PROJECT_ROOT, templatename + "/templates"),
-            os.path.join(PROJECT_ROOT, "templates")
+            os.path.join(PROJECT_ROOT, templatename + "/templates"),
+            # os.path.join(PROJECT_ROOT, "templates")
         ],
         "APP_DIRS": True,
         "OPTIONS": {
