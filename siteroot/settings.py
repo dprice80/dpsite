de06b7af59b1b83758ff19e7b590eb1e6da8769c
Darren Price <dprice80@gmail.com>
2017-03-02 19:27:08 -0500

    updates templates

--- siteroot/settings.py
+++ siteroot/settings.py
@@ -198,6 +198,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
+            os.path.join(PROJECT_ROOT, "mytemplates/templates"),
             os.path.join(PROJECT_ROOT, "templates")
         ],
         "APP_DIRS": True,
